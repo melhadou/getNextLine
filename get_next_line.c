0586804fb738bd2e46bc059bdfefbf86d96cf2d3
Mohamed Elhadouchi <melhadou@student.1337.ma>
2023-01-17 13:02:05 +0100

    refactor some code

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 17:12:44 by melhadou          #+#    #+#             */
-/*   Updated: 2023/01/13 16:42:35 by melhadou         ###   ########.fr       */
+/*   Updated: 2023/01/15 19:09:22 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,48 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-char *backed_up(char *backup,char *str)
+char *read_buffer(int fd, char *backup)
 {
-	backup = ft_strjoin(backup, str);
-	return (backup);
-}
-
-char *read_from_file(int fd)
-{
-	static char *backup = "";
-	char *af_n;
-	char *tmp;
 	char *buf;
-	int len;
+	char *line;
+	int tmp;
 	int r;
 
-	r = 1;
+	buf = NULL;
 	if (!(buf = malloc((sizeof(char *) * BUFFER_SIZE ) + 1)))
 		return (NULL);
-	len = -1;
-	while (++len < BUFFER_SIZE)
-		buf[len] = 0;
-	while (ft_strchr(buf, '\n') == 0 && r != 0)
+	while(1)
 	{
 		r = read(fd, buf, BUFFER_SIZE);
 		if (r == -1)
 		{
 			free(buf);
-			free(backup);
 			return (NULL);
 		}
 		buf[BUFFER_SIZE] = '\0';
-		len = ft_strchr(buf, '\n');
-		if (len == 0)
-		{
-			backup = backed_up(backup, buf);
-		}
+		tmp = ft_strchr(buf, '\n');
+		if (r == 0 || tmp == 0)
+			break;
 		else
 		{
-			break;
+			line = ft_strjoin(buf, line);
 		}
 	}
-	af_n = ft_substr(buf, len + 1, -1);
-	tmp = ft_strjoin(backup, ft_substr(buf, 0, len + 1));
-	backup = af_n;
-	return (tmp);
+	free(buf);
+	return line;
 }
 
 char	*get_next_line(int fd)
 {
+	static char *backup = "";
 	char *line;
+	char *buf;
+	int tmp;
+	int r;
+
 	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
-	line = read_from_file(fd);
+	line = read_buffer(fd, backup);
 	return line;
 }
 
@@ -78,16 +67,16 @@ int main (int argc, char *argv[])
 
 	char *cha = get_next_line(fd);
 	char *cha1 = get_next_line(fd);
-	char *cha2 = get_next_line(fd);
-	char *cha3 = get_next_line(fd);
+	// char *cha2 = get_next_line(fd);
+	// char *cha3 = get_next_line(fd);
 
 	printf("%s",cha);
 	printf("%s",cha1);
-	printf("%s",cha2);
-	printf("%s",cha3);
+	// printf(", %s",cha2);
+	// printf(", %s",cha3);
 	free(cha);
 	free(cha1);
-	free(cha2);
-	free(cha3);
+	// free(cha2);
+	// free(cha3);
 	return 0;
 }
